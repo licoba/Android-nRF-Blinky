3671af01e8ab9331f3c37318ca2d93729de7cde9
licoba <licoba@qq.com>
2024-01-24 18:13:55 +0800

    feat：修改UUID

--- blinky/spec/src/main/java/no/nordicsemi/android/blinky/spec/BlinkySpec.kt
+++ blinky/spec/src/main/java/no/nordicsemi/android/blinky/spec/BlinkySpec.kt
@@ -3,11 +3,31 @@ package no.nordicsemi.android.blinky.spec
 import java.util.UUID
 
 class BlinkySpec {
-
     companion object {
         val BLINKY_SERVICE_UUID: UUID = UUID.fromString("00001523-1212-efde-1523-785feabcd123")
         val BLINKY_BUTTON_CHARACTERISTIC_UUID: UUID = UUID.fromString("00001524-1212-efde-1523-785feabcd123")
         val BLINKY_LED_CHARACTERISTIC_UUID: UUID = UUID.fromString("00001525-1212-efde-1523-785feabcd123")
     }
 
+}
+
+enum class ScanUUIDFilter(val uuidString: String) {
+    WT2(UUIDWith("6f25")),
+    WT2_BK(UUIDWith("6f26")),
+    M2(UUIDWith("6f27")),
+    WT2_Edge(UUIDWith("6f28")),
+    WT2_Edge_Fac(UUIDWith("6f20")),
+    W_UPGRADE(StandardUUIDWith("FE59")),
+    M2P(UUIDWith("6f29")),
+    M3(UUIDWith("6f2a")),
+    W3Pro(UUIDWith("6f31")),
+    W3Pro_Fac(UUIDWith("6f32"));
+}
+
+fun UUIDWith(channel: String): String {
+    return "592e$channel-432f-4669-84b1-ddc45cc40dd9"
+}
+
+fun StandardUUIDWith(channel: String): String {
+    return "0000$channel-0000-1000-8000-00805f9b34fb"
 }
--- blinky/ui/src/main/java/no/nordicsemi/android/blinky/ui/scanner/view/BlinkyScanner.kt
+++ blinky/ui/src/main/java/no/nordicsemi/android/blinky/ui/scanner/view/BlinkyScanner.kt
@@ -8,6 +8,7 @@ import no.nordicsemi.android.blinky.ui.R
 import no.nordicsemi.android.scanner.DeviceSelected
 import no.nordicsemi.android.scanner.ScannerScreen
 import no.nordicsemi.android.blinky.spec.BlinkySpec
+import no.nordicsemi.android.blinky.spec.ScanUUIDFilter
 
 @Composable
 fun BlinkyScanner(
@@ -15,7 +16,7 @@ fun BlinkyScanner(
 ) {
     ScannerScreen(
         title = stringResource(id = R.string.scanner_title),
-        uuid = ParcelUuid(BlinkySpec.BLINKY_SERVICE_UUID),
+        uuid = ParcelUuid.fromString(ScanUUIDFilter.WT2_Edge.uuidString),
         cancellable = false,
         onResult = { result ->
             when (result) {
